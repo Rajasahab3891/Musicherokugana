cfd4044ec3e1ab05b4804bf354366432ea54f2c1
Rajasahab3891 <162156709+Rajasahab3891@users.noreply.github.com>
2024-09-17 16:41:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,8 +45,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Pandababyupdates")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PandaLovebaby")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Sanatani_Vibes")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sanatani_Vibes")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -84,7 +84,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/c3e75a195cdda1380a176.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/41a7bfb511fec60c8c6a4.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/5bf629d10afd4af953585.jpg"
